b51018bd609fbf093c6cc4c0429c4a2d63318f6a
GayanShyamal <gayanshyamal706@gmail.com>
2026-04-25 19:09:56 +0530

    Lecture practical

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
     static void main() {
-        System.out.println("I love Java");
+//        System.out.println("I love Java");
+//        System.out.println("Shopping list:");
+//        System.out.println("\t\t\t\tApple");
+//        System.out.println("\t\t\t\tBanana");
+//        System.out.println("\t\t\t\tLow-fat-milk");
+
+//        String country;
+//        country ="sri lanaka";
+//        String country = "Sri lanka";
+//
+//        System.out.println("I love "+country);
+//        System.out.println(country.indexOf("l"));
+//        System.out.println(country.indexOf("a"));
+
+//        System.out.println(country.substring(0,6));
+//        System.out.println(country.substring(2,8));
+
+//        System.out.println(country.length() );
+
+//        String java;
+//        java = "I love Java and Java Loves me.";
+//        System.out.println(java.indexOf("Love"));
+//        System.out.println(java.indexOf("Java"));
+
+//        System.out.println(country +"!" + java);
+
+//        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("Eneter your name:");
+//        String name = scanner.next();
+//        System.out.println("Hi "+name);
+//        System.out.println("Enetr your age:");
+//        int age = scanner.nextInt();
+//        System.out.println("Age is "+age);
+
+//        System.out.println("Enter your First name:");
+//        String first_name = scanner.next();
+//        System.out.println("Enetr your middle name:");
+//        String middle_name = scanner.next();
+//        System.out.println("Enetr your last name:");
+//        String last_name = scanner.next();
+//
+//        System.out.println(first_name.substring(0,1)+"."+middle_name.substring(0,1)+"."+last_name);
+//        System.out.println("first name is "+first_name);
+//        System.out.println("Full name is "+first_name+" "+middle_name+" "+last_name);
+
+
+
+
 
     }
 }
