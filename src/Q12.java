4fd46429640d414c92b067ec4802fa23b41b1334
GayanShyamal <gayanshyamal706@gmail.com>
2026-04-26 00:04:06 +0530

    Done Q12

---
+++ src/Q12.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Q12 {
+    public static void main(String[] args) {
+
+        JFrame myWindow = new JFrame();
+
+        myWindow.setTitle("My First Frame");
+        myWindow.setSize(300,200);
+        myWindow.setVisible(true);
+        myWindow.setLocation(50,50);
+
+    }
+}
