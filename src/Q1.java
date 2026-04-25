bc43dc2f8bbec64b00fafacfb101e943f7048501
GayanShyamal <gayanshyamal706@gmail.com>
2026-04-25 18:00:01 +0530

    Do Q1 in lab sheet

---
+++ src/Q1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Q1 {
+   public static void main() {
+       JFrame myWindow  = new JFrame();
+       myWindow.setSize(800,600);
+       myWindow.setVisible(true);
+//       myWindow.setTitle("Welcome to Java" );
+
+   }
+}
