dc25fadd0ef5d13425fee2dbab0cd01719aefb36
GayanShyamal <gayanshyamal706@gmail.com>
2026-04-26 15:04:48 +0530

    Done Q13

--- src/Q13.java
+++ src/Q13.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 public class Q13 {
     public static void main(String[] args) {
-
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(500,200);
+        myWindow.setTitle("UOK");
+        myWindow.setVisible(true);
+        try{
+            Thread.sleep(500);
+        }
+        catch (Exception e){}
+        myWindow.setVisible(false);
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e){}
+        myWindow.setVisible(true);
     }
 }
